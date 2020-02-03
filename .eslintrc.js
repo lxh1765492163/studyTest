e7469ce278f53bf5933a72fc17c606327b91f0f0
luoxiaohong <1765492163@qq.com>
2020-02-04 00:02:09 +0800

    test

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: ['plugin:prettier/recommended'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+console.log('执行了检测-----------4');
+
+const bb = { a: '2', dasdasd: '21312313', adasd: 'asdasd', asd: 'asdsada' };
+
+console.log('执行了检测------------2', bb);
